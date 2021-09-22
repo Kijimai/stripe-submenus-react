ecaef18b9bf38bcc0e3f56ddcc9ae9db56ac1c59
Jibbyworks <JDBucog@yahoo.com>
2021-09-21 17:36:32 -0700

    add sidebar and hero

--- src/context.js
+++ src/context.js
@@ -1,2 +1,45 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react"
+import sublinks from "./data"
+
+const AppContext = React.createContext()
+
+export const AppProvider = ({ children }) => {
+  const [isSideBarOpen, showSideBar] = useState(false)
+  const [isSubmenuOpen, showSubmenu] = useState(true)
+
+  const openSidebar = () => {
+    showSideBar(true)
+  }
+
+  const closeSidebar = () => {
+    showSideBar(false)
+  }
+
+  const openSubmenu = () => {
+    showSubmenu(true)
+  }
+
+  const closeSubmenu = () => {
+    showSubmenu(false)
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        openSidebar,
+        closeSubmenu,
+        openSubmenu,
+        closeSidebar,
+        isSubmenuOpen,
+        isSideBarOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+//custom hook
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
